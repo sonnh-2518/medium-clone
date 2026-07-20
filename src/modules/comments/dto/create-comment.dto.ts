2ebb7fe8069d1a1a1f3b212d0fbd0e04af6bbaa1
sonnh-2518 <nguyen.hong.son-b@sun-asterisk.com>
2026-07-20 23:47:55 +0700

    feat: implement CRD common on article

--- src/modules/comments/dto/create-comment.dto.ts
+++ src/modules/comments/dto/create-comment.dto.ts
@@ -1,3 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { i18nValidationMessage } from 'nestjs-i18n';
+
 export class CreateCommentDto {
+  @ApiProperty({ example: 'His name was my name too.' })
+  @IsString({ message: i18nValidationMessage('common.validation.IS_STRING') })
+  @IsNotEmpty({
+    message: i18nValidationMessage('common.validation.IS_NOT_EMPTY'),
+  })
   body: string;
 }
